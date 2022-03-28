7c634d0daf3499a073fff2836b4e5219885cdb8d
InaiaKenia <inaiakenia15@gmail.com>
2022-03-27 18:17:49 -0600

    APIKey

--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,11 @@ function homePage() {
     var clearEl = document.getElementById("clear-history");
     var currentPicEl = document.getElementById("current-pic");
     var currentTempEl = document.getElementById("temperature");
-    var currentHumidityEl = document.getElementById("humidity");4
+    var currentHumidityEl = document.getElementById("humidity");
     var currentWindEl = document.getElementById("wind-speed");
     var currentUVEl = document.getElementById("UV-index");
     var historyEl = document.getElementById("history");
     let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
     console.log(searchHistory);
+
+    var APIKey = "f49f37f87b0f4b82ac0459bf392a3020";
